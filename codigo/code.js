6eab117865bc7d6f333ab3efe7ab97a1ac0fd28f
zAdreno <80124726+zAdreno@users.noreply.github.com>
2024-05-03 03:03:32 +0200

    Delete codigo directory

--- codigo/code.js
+++
@@ -1,8 +0,0 @@
-const MONTH_CODES = ['BB', 'LS', 'JH', 'PL', 'BK', 'WH', 'FF', 'BF', 'CF', 'CK', 'CB', 'VM'];
-const d = new Date();
-const month = d.getMonth();
-const monthCode = MONTH_CODES[month];
-const randomNums = Math.floor(Math.random() * 0x15f90) + 0x2710;
-window.onload = function () {
-  document.getElementsByTagName("codigo")[0x0].innerHTML = monthCode + randomNums;
-};
